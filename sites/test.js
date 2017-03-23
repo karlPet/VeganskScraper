0c5c47fddd3dd73fa8f9fa5dc47ea619b2c85a80
karlpet <karlpetter.svensson@hotmail.se>
2017-03-23 22:20:08 +0100

    added gitignore

--- sites/test.js
+++ sites/test.js
@@ -1,6 +1,6 @@
 var scraper = require(__dirname + "/scraper");
 
 scraper().then(result => {
-  //console.log(result);
+  console.log(result);
   console.log("total amount of recipes: ", result.length);
 });
