0042f8070222e1bc0d3e9588d7c0d9fa4b942c8b
karlpet <karlpetter.svensson@hotmail.se>
2017-03-23 23:22:00 +0100

    updated gitignore

--- sites/koket/index.js
+++ sites/koket/index.js
@@ -3,20 +3,30 @@ var Xray = require("x-ray");
 var xray = Xray();
 
 var internals = {
-    recipeId: "article",
+    recipeId: ".list-item.recipe",
     recipeNamesId: "a img@alt",
-    recipeLinksId: "a@href"
+    recipeLinksId: "a@href",
+    recipeImgId: "a img@src",
+    alternateRecipeImage: "a noscript img@src"
 };
 
 var scrapeWebsite = () => new Promise(function(resolve, reject) {
 
     xray(mapping.links[0], internals.recipeId, [{
         recipeName: internals.recipeNamesId,
-        recipeLink: internals.recipeLinksId
+        recipeLink: internals.recipeLinksId,
+        recipeImage: internals.recipeImgId,
+        alternateRecipeImage: internals.alternateRecipeImage
     }])((err, obj) => {
         if (err) {
             reject(err);
         }
+        obj.filter(n => n.alternateRecipeImage)
+           .forEach(n => {
+               n.recipeImage = n.alternateRecipeImage;
+               delete n.alternateRecipeImage;
+           });
+
         resolve(obj);
     });
 });
