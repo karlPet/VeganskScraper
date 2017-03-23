78d2fcecd3462454ed5d4632c483189efa9f67c7
karlpet <karlpetter.svensson@hotmail.se>
2017-03-24 00:19:42 +0100

    updated more web pagess....

--- sites/tasteline/index.js
+++ sites/tasteline/index.js
@@ -6,21 +6,38 @@ var internals = {
     paginate: "",
     recipeId: ".recipe-header",
     recipeNamesId: "h2",
-    recipeLinksId: "a@href"
+    recipeLinksId: "a@href",
+    recipeImgId: ".recipe-header-image a img@src"
 };
 
-var scrapeWebsite = () => new Promise(function(resolve, reject) {
+var scrapeWebsites = () => new Promise((resolve, reject) => {
 
-    xray(mapping.links[0], internals.recipeId, [{
+    Promise.all(mapping.links.map(site => scrapeWebsite(site)))
+    .then(recipeData =>
+        resolve(recipeData.reduce((a, b) => a.concat(b)) // reduce just merges all arrays of recipes into one array
+    ));
+});
+
+
+var scrapeWebsite = (site) => new Promise(function(resolve, reject) {
+
+    xray(site, internals.recipeId, [{
         recipeName: internals.recipeNamesId,
-        recipeLink: internals.recipeLinksId
+        recipeLink: internals.recipeLinksId,
+        recipeImage: internals.recipeImgId
     }]).paginate(internals.paginate)(function(err, obj) {
 
         if (err) {
             reject(err);
         }
+
+        obj.filter(n => n.recipeImage === 'http://cdn1.tasteline.com/Food_missing-336x222.jpg')
+           .forEach(n => delete n.recipeImage);
+
         resolve(obj);
     });
 });
 
-module.exports = scrapeWebsite;
+
+
+module.exports = scrapeWebsites;
